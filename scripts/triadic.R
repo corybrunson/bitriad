bd40c283b1921b013d2bf8931a627003fe4bea9e
corybrunson <brunson@uchc.edu>
2014-06-10 11:51:46 -0400

    Errors

--- scripts/triadic.R
+++ scripts/triadic.R
@@ -1,12 +1,14 @@
+mysrc <- function(src.file) source(paste(mydir, src.file, sep = ''))
+
 # igraph library
 library(igraph)
 
 # Indexing scheme
-myfn('partition.bijections.R')
+mysrc('partition.bijections.R')
 
 # Candidate clustering coefficients
-myfn('twomode.transitivity.R')
+mysrc('twomode.transitivity.R')
 
 # Triad census functions
-myfn('twomode.triad.census.R')
-myfn('tc2ccs.R')
+mysrc('twomode.triad.census.R')
+mysrc('tc2ccs.R')
