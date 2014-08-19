384124ca07febf372e015cf491880034388eb83b
corybrunson <brunson@uchc.edu>
2014-08-19 14:35:35 -0400

    replaced structural.triad.census with better se.triad.census

--- triadic/R/simple.triad.census.R
+++ triadic/R/simple.triad.census.R
@@ -1,26 +1,35 @@
-#' Two-mode triad census
+#' Simple triad census
 #' 
-#' These functions compute triad censuses for affiliation networks. (Currently
-#' only the full triad census is implemented and the structural and simple
-#' censuses are computed from it.) The full and structural censuses return
-#' matrices, the former of whatever dimension is needed to contain the census
-#' and the latter 4-by-2. The simple census returns a vector of length 4.
-#' @param bigraph The ambient affiliation network
-#' @param graph A one-mode network (used as the one-mode projection of bigraph)
-#' @param type The actor node type in bigraph
-#' @param rcnames Logical; whether to label the matrix rows and columns
-#' @param verbose Logical; whether to display progress bars
-#' @param tmtc A full two-mode triad census (not necessarily labeled)
+#' This function inputs a simple graph and returns a vector of frequency counts
+#' for the four isomorphism classes of triad, which therefore sum to
+#' choose(n, 3), where n is the number of nodes. This function is needed because
+#' igraph::triad.census, from which in small cases a simple triad census can be
+#' recovered through strategic coordinate sums, returns nonsense frequencies for
+#' many large graphs.
+#' @param graph A one-mode network
+#' @param rcnames Logical; whether to label the vector coordinates (0 thru 3)
 #' @export
 
 simple.triad.census <-
-function(graph, rcnames = FALSE) {
-        tc <- triad.census(as.directed(graph))
-        stopifnot(sum(tc) == choose(vcount(graph), 3))
-        if(is.nan(tc[1])) {
-            tc[1] <- choose(vcount(graph), 3) - sum(tc, na.rm = TRUE)
+    function(graph) {
+        # Use implemented triad census if it makes sense
+        C <- triad.census(as.directed(graph))
+        if(sum(C) == choose(vcount(graph), 3) & all(C >= 0) & !is.nan(C[1])) {
+            C <- C[c(1, 3, 11, 16)]
+            names(C) <- 0:3
+            return(C)
         }
-        stc <- tc[c(1, 3, 11, 16)]
-        if(rcnames) names(stc) <- 0:3
-        return(stc)
+        # Initialize census and graph size
+        n <- vcount(graph)
+        # Across edges, tally nodes adjacent to 0, 1, or 2 of the endpoints
+        edge.plus <- apply(get.edgelist(graph), 1, function(pair) {
+            nbhd <- neighborhood(graph, 1, pair)
+            cons <- length(unique(unlist(nbhd))) - 2
+            tris <- length(do.call(intersect, nbhd)) - 2
+            c('1' = n - cons - 2, '2' = cons - tris, '3' = tris)
+        })
+        # Store C as row sums, correct for repeats, fill in empty triad count
+        C <- c('0' = 0, rowSums(edge.plus) / 1:3)
+        C[1] <- choose(n, 3) - sum(C)
+        return(C)
     }
