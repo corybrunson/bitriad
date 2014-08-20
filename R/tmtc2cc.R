f6a19bb682b37b0de4c7cd08679001c922510948
corybrunson <brunson@uchc.edu>
2014-08-20 17:30:56 -0400

    proofing7

--- R/tmtc2cc.R
+++ R/tmtc2cc.R
@@ -16,9 +16,9 @@ function(tc, S.fn, F.fn, num.denom = FALSE) {
     S.c <- sum(sapply(1:dim(tc)[2], function(j) sapply(
         1:dim(tc)[1], function(i) {
             if(tc[i, j] == 0) 0 else
-                S.fn(index.partition(3, i - 1), j - 1) * tc[i, j]})))
+                S.fn(index.partition(i - 1), j - 1) * tc[i, j]})))
     F.c <- sum(sapply(1:dim(tc)[2], function(j) sapply(
         1:dim(tc)[1], function(i) {
             if(tc[i, j] == 0) 0 else
-                F.fn(index.partition(3, i - 1), j - 1) * tc[i, j]})))
+                F.fn(index.partition(i - 1), j - 1) * tc[i, j]})))
     return(if(num.denom) c(S.c, S.c + F.c) else S.c / (S.c + F.c))}
