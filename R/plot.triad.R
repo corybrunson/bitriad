3db1f504853e3a2157ca90247757d19166063273
corybrunson <brunson@uchc.edu>
2014-09-08 16:10:12 -0400

    never mind

--- R/plot.triad.R
+++ R/plot.triad.R
@@ -5,15 +5,14 @@
 #' @export
 
 plot.triad <-
-function(lambda, w, S = .001, layout,
-         scale = 0.3, angdir = -1, rot = -pi/2,
-         rot.lambda = c(0, 0, 0), rot.w = pi/12, ...) {
+function(lambda, w, layout, scale = 0.3, angdir = -1,
+         rot = -pi/2, rot.lambda = c(0, 0, 0), rot.w = pi/12, ...) {
     tr <- twomode.triad(lambda, w)
     q <- sum(c(lambda, w))
     # Default layout
-    if(missing(layout)) layout <- wobble.coords(layout.triad(
+    if(missing(layout)) layout <- layout.triad(
         lambda = lambda, w = w, scale = scale, angdir = angdir,
-        rot = rot, rot.lambda = rot.lambda, rot.w = rot.w), S = S)
+        rot = rot, rot.lambda = rot.lambda, rot.w = rot.w)
     # Plot graph in a slightly widened frame
     plot(tr, layout = layout, xlim = c(-1.3, 1.3), ylim = c(-1.3, 1.3),
          vertex.label = V(tr)$name,
