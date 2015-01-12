d10404424ee9b673c40779f4728a0f7d251d3f1d
corybrunson <brunson@uchc.edu>
2015-01-12 11:24:45 -0500

    update data, in particular assign correct times to DDGGS1

--- R/is.dynamic.R
+++ R/is.dynamic.R
@@ -3,6 +3,7 @@
 #' A simple graph is dynamic if its edges have time stamps. A bipartite graph is
 #' dynamic if its nodes (most importantly, its event nodes) have time stamps.
 #' @param graph An igraph object
+#' @export
 
 is.dynamic <-
     function(graph) {
