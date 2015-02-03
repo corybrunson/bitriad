703c421da09af56ede7e7267ea49f5dcf879759b
corybrunson <brunson@uchc.edu>
2015-02-03 08:01:31 -0500

    test and coerce affiliation network objects

---
+++ R/as.an.R
@@ -0,0 +1,19 @@
+#' Affiliation network
+#' 
+#' @param graph A graph object
+#' @export
+
+as.an <-
+function(graph) {
+    # Must be a graph object
+    if(!is.igraph(graph)) stop('Not an igraph object')
+    # Must have node types (i.e. be "bipartite")
+    if(!('type' %in% list.vertex.attributes(graph)))
+        stop('Needs type attribute')
+    # There must be no edges between nodes of the same type
+    el <- get.edgelist(graph, names = FALSE)
+    if(!all(V(graph)$type[el[, 1]] + V(graph)$type[el[, 2]]))
+        stop('Type attribute does not form a bipartition')
+    # Vertices must be in order of type
+    permute.vertices(graph, order(V(graph)$type))
+}
---
+++ R/is.an.R
@@ -0,0 +1,17 @@
+#' Affiliation network
+#' 
+#' @param graph A graph object
+#' @export
+
+is.an <-
+function(graph) {
+    # Must be a graph object
+    if(!is.igraph(graph)) return(FALSE)
+    # Must have node types (i.e. be "bipartite")
+    if(!('type' %in% list.vertex.attributes(graph))) return(FALSE)
+    # Vertices must be in order of type
+    if(!all(V(graph)$type == sort(V(graph)$type))) return(FALSE)
+    # There must be no edges between nodes of the same type
+    el <- get.edgelist(graph, names = FALSE)
+    all(V(graph)$type[el[, 1]] + V(graph)$type[el[, 2]])
+}
