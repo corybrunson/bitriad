cfdb0d0b21d16e8ff9a3c0270b8eee29cfab2ab8
Cory Brunson <brunson@uchc.edu>
2017-08-02 15:41:38 -0400

    clean up mode-specific functions

--- R/mode-counts.R
+++ R/mode-counts.R
@@ -1,30 +1,32 @@
-#' Count the actors and events in an affiliation network
-#' 
-#' These functions return the number of actors (nodes of type "FALSE") or events
-#' ("TRUE") in an affiliation network.
-#' 
+#' @title Count the actors and events in an affiliation network
+#'   
+#' @description These functions return the number of actors (nodes with
+#'   \code{type} attribute \code{FALSE}) or events (\code{TRUE}) in an
+#'   affiliation network.
+#'   
 #' @name mode_counts
-#' @param bigraph An affiliation network; see \code{\link{is_an}}.
+#' @param bigraph An affiliation network.
 #' @examples
 #' data(chicago1960s)
 #' actor_count(chicago1960s)
 #' event_count(chicago1960s)
-#' @export
-actor_count <-
-  function(bigraph) {
-    if(vcount(bigraph) == 0) return(0)
-    if(!is_an(bigraph)) stop('Not an affiliation network')
-    length(which(!V(bigraph)$type))
-  }
+NULL
 
 #' @rdname mode_counts
 #' @export
-event_count <-
-  function(bigraph) {
-    if(vcount(bigraph) == 0) return(0)
-    if(!is_an(bigraph)) stop('Not an affiliation network')
-    length(which(V(bigraph)$type))
-  }
+actor_count <- function(bigraph) {
+  stopifnot(is_an(bigraph))
+  if (vcount(bigraph) == 0) return(0)
+  length(which(!V(bigraph)$type))
+}
+
+#' @rdname mode_counts
+#' @export
+event_count <- function(bigraph) {
+  stopifnot(is_an(bigraph))
+  if (vcount(bigraph) == 0) return(0)
+  length(which(V(bigraph)$type))
+}
 
 #' @rdname mode_counts
 #' @export
---
+++ R/mode-projection.r
@@ -0,0 +1,56 @@
+#' @title Project an affiliation network onto its actors
+#'   
+#' @description These functions use \code{\link{bipartite_projection}} to 
+#'   compute the projections of an affiliation network onto the actor or event 
+#'   nodes.
+#'   
+#' @name mode_projection
+#' @param bigraph An affiliation network.
+#' @param mode Numeric or character; whether to project onto actors (\code{1} or
+#'   \code{"actors"}) or onto events (\code{2} or \code{"events"}).
+#' @param name Character; the attribute of the actor or event nodes in 
+#'   \code{bigraph} to use as names for the nodes in the projection.
+#' @examples
+#' data(chicago1960s)
+#' tab <- table(V(chicago1960s)$type)
+#' proj <- actor_projection(chicago1960s)
+#' vcount(proj) == tab[1]
+#' proj <- event_projection(chicago1960s)
+#' vcount(proj) == tab[2]
+#' @export
+mode_projection <- function(bigraph, mode = "actors", name = "name") {
+  stopifnot(is_an(bigraph))
+  if (is.character(mode)) {
+    mode <- match.arg(mode, c("actors", "events"))
+    mode <- if (mode == "actors") 1 else 2
+  } else {
+    mode <- as.numeric(mode)
+  }
+  if (vcount(bigraph) == 0) return(make_empty_graph(directed = FALSE))
+  V(bigraph)$name <- if (name == "id") {
+    V(bigraph)
+  } else {
+    get.vertex.attribute(bigraph, name)
+  }
+  bipartite_projection(bigraph, multiplicity = TRUE)[[mode]]
+}
+
+#' @rdname mode_projection
+#' @export
+actor_projection <- function(bigraph, name = "name") {
+  mode_projection(bigraph = bigraph, name = name, mode = 1)
+}
+
+#' @rdname mode_projection
+#' @export
+event_projection <- function(bigraph, name = "name") {
+  mode_projection(bigraph = bigraph, name = name, mode = 2)
+}
+
+#' @rdname mode_projection
+#' @export
+actor.projection <- actor_projection
+
+#' @rdname mode_projection
+#' @export
+event.projection <- event_projection
