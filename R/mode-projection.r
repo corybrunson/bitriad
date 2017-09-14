0e589bf65e49b244b16b60807def87261ad3ad60
Cory Brunson <brunson@uchc.edu>
2017-09-14 08:41:33 -0400

    debug mode-projection; generalize mode-addition

--- R/mode-projection.r
+++ R/mode-projection.r
@@ -5,13 +5,15 @@
 #'   nodes.
 #'   
 #' @name mode_projection
-#' @seealso Original \strong{igraph} functions:
+#' @seealso Original \strong{igraph} functions: 
 #'   \code{\link[igraph]{bipartite_projection}}
 #' @param graph An affiliation network.
 #' @param mode Numeric or character; whether to project onto actors (\code{1} or
 #'   \code{"actors"}) or onto events (\code{2} or \code{"events"}).
 #' @param name Character; the attribute of the actor or event nodes in 
-#'   \code{graph} to use as names for the nodes in the projection.
+#'   \code{graph} to use as names for the nodes in the projection. If \code{NA},
+#'   node IDs are converted to characters and used. If \code{NULL}, no names are
+#'   assigned.
 #' @examples
 #' data(chicago1960s)
 #' tab <- table(V(chicago1960s)$type)
@@ -20,7 +22,7 @@
 #' proj <- event_projection(chicago1960s)
 #' vcount(proj) == tab[2]
 #' @export
-mode_projection <- function(graph, mode = "actors", name = "name") {
+mode_projection <- function(graph, mode = 1, name = "name") {
   stopifnot(is_an(graph))
   if (is.character(mode)) {
     mode <- match.arg(mode, c("actors", "events"))
@@ -29,10 +31,19 @@ mode_projection <- function(graph, mode = "actors", name = "name") {
     mode <- as.numeric(mode)
   }
   if (vcount(graph) == 0) return(make_empty_graph(directed = FALSE))
-  V(graph)$name <- if (name == "id") {
-    V(graph)
-  } else {
-    get.vertex.attribute(graph, name)
+  if ("name" %in% names(match.call()) & !is.null(name)) {
+    if (!is.na(name) & !(name %in% names(vertex_attr(graph)))) {
+      warning("Attribute '", name, "' not found; ",
+              "using default naming scheme.")
+      name <- NULL
+    }
+  }
+  if (!is.null(name)) {
+    V(graph)$name <- if (is.na(name)) {
+      V(graph)
+    } else {
+      get.vertex.attribute(graph, name)
+    }
   }
   bipartite_projection(graph, multiplicity = TRUE)[[mode]]
 }
