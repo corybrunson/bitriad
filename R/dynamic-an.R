06230e82ef6be999a718cf7b8117889e4fe41eb5
Cory Brunson <brunson@uchc.edu>
2017-10-31 07:37:39 -0400

    debug as_dynamic_an

--- R/dynamic-an.R
+++ R/dynamic-an.R
@@ -7,17 +7,19 @@
 #' @name dynamic_an
 #' @family network testing and coercion
 #' @param graph An affiliation network.
-#' @param add.time.attribute Character or logical; if character, the existing 
-#'   attribute to coerce to numeric if necessary and use as the time attribute;
-#'   if logical, whether to introduce an artificial \code{time} attribute
-#'   \code{1:event_count(graph)}, reflecting the order of the event node IDs,
-#'   if \code{graph} has none.
+#' @param use.time.attribute Character; the vertex attribute to coerce to 
+#'   numeric if necessary and use as the \code{"time"} attribute.
+#' @param add.time.attribute Logical; whether, if \code{graph} has no attribute
+#'   \code{use.time.attribute}, to introduce an artificial \code{"time"}
+#'   attribute taking the values \code{1:event_count(graph)}, reflecting the
+#'   order of the event node IDs.
 NULL
 
 #' @rdname dynamic_an
 #' @export
 is_dynamic_an <- function(graph) {
   if (!is_an(graph)) return(FALSE)
+  if (vcount(graph) == 0) return(TRUE)
   if (!("time" %in% vertex_attr_names(graph))) return(FALSE)
   if (!is.numeric(vertex_attr(graph, "time"))) return(FALSE)
   !(is.unsorted(vertex_attr(graph, "time",
@@ -34,29 +36,37 @@ is.dyn <- function(graph) {
 
 #' @rdname dynamic_an
 #' @export
-as_dynamic_an <- function(graph, add.time.attribute = FALSE) {
-  if (!is_an(graph)) stop("Not an affiliation network.")
-  if (!("time" %in% vertex_attr_names(graph))) {
-    if (is.character(add.time.attribute)) {
-      V(graph)$time <- c(
-        rep(NA, actor_count(graph)),
-        as.numeric(vertex_attr(graph, add.time.attribute,
-                               V(graph)[V(graph)$type == TRUE]))
-      )
-    } else if (add.time.attribute) {
-      V(graph)$time <- c(rep(NA, actor_count(graph)),
-                           1:event_count(graph))
+as_dynamic_an <- function(
+  graph, use.time.attribute = "time", add.time.attribute = FALSE
+) {
+  if (!is_igraph(graph)) stop("Not an igraph object.")
+  if (vcount(graph) == 0) return(graph)
+  graph <- as_an(graph)
+  
+  if (!use.time.attribute %in% vertex_attr_names(graph)) {
+    if (add.time.attribute) {
+      warning("No vertex attribute '", use.time.attribute, "'")
+      set_vertex_attr(graph, "time",
+                      c(rep(NA, actor_count(graph)), 1:event_count(graph)))
+      use.time.attribute <- "time"
     } else {
-      stop("Needs 'time' attribute.")
-    }
-  } else {
-    if (is.character(add.time.attribute)) {
-      warning("'graph' already has a 'time' attribute.")
+      stop("No vertex attribute '", use.time.attribute, "'")
     }
   }
+  
+  if (use.time.attribute != "time") {
+    if ("time" %in% vertex_attr_names(graph)) {
+      stop("'graph' has a 'time' attribute.")
+    }
+    set_vertex_attr(graph, "time",
+                    c(rep(NA, actor_count(graph)),
+                      as.numeric(vertex_attr(graph, add.time.attribute,
+                                             V(graph)[V(graph)$type == TRUE]))))
+  }
+  
   permute(graph, c(
     1:actor_count(graph),
     actor_count(graph) +
-      order(order(V(graph)[V(graph)$type == TRUE]$time)))
+      order(order(vertex_attr(graph, "time")[V(graph)$type == TRUE])))
   )
 }
